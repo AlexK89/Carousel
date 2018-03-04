f0ba2190e7ccf1b16fbf1d3f59fabafaea1e946c
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-04 22:28:48 +0000

    carousel - v.0.2

--- js/main.js
+++ js/main.js
@@ -3,31 +3,36 @@ const carouselArrows = document.querySelectorAll('.carousel__arrows');
 function carousel(carouselArrow, direction) {
 	const carouselImages = carouselArrow.parentNode.querySelectorAll('.images img');
 
-	console.log(carouselImages);
 	for (let i = 0; i < carouselImages.length; i++) {
 		if (carouselImages[i].classList.contains('active')) {
 			carouselImages[i].classList.remove('active');
 
 			if (direction >= 0) {
-				if (carouselImages[i+1] === undefined) {
-					carouselImages[0].classList.add('active');
-				} else {
-					carouselImages[i+1].classList.add('active');
-				}
+				moveToTheRight(carouselImages, i);
 			} else {
-				if (carouselImages[i-1] === undefined) {
-					carouselImages[carouselImages.length - 1].classList.add('active');
-				} else {
-					carouselImages[i-1].classList.add('active');
-				}
+				moveToTheLeft(carouselImages, i);
 			}
 			break;
 		}
 	}
 }
 
+function moveToTheLeft(carouselImages, i) {
+	if (carouselImages[i-1] === undefined) {
+		carouselImages[carouselImages.length - 1].classList.add('active');
+	} else {
+		carouselImages[i-1].classList.add('active');
+	}
+}
+
+function moveToTheRight(carouselImages, i) {
+	if (carouselImages[i+1] === undefined) {
+		carouselImages[0].classList.add('active');
+	} else {
+		carouselImages[i+1].classList.add('active');
+	}
+}
 function changeImage(arrow) {
-	console.log(arrow);
 	if (arrow.classList.contains('left_arrow')) {
 		carousel(arrow , -1);
 	}
